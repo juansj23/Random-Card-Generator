1a2a0a03f7279056041bbcd977cc9d8bee679fb1
juansj23 <jcsj24@gmail.com>
2023-03-20 20:52:46 +0000

    Funciones actualizadas

--- src/app.js
+++ src/app.js
@@ -9,3 +9,30 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+//Funcion para generar las cartas random
+let generadorRandomCards = () => {
+  let suit = ["♦", "♥", "♠", "♣"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
+
+//Funcion para generar los numeros random
+let generadorRandomNumbers = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
